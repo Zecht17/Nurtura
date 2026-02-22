190eead08a1380e1d1b967890a3d28cbf74fd80c
Zecht17 <jm.bayucan@gmail.com>
2026-02-22 18:48:03 +0800

    Fixed the nav bar after adding the task page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,54 @@
 import Feather from '@expo/vector-icons/Feather';
+import Octicons from "@expo/vector-icons/Octicons";
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
   return (
-      <Tabs screenOptions={{tabBarActiveTintColor: "#7C6FDC"}}>
-        <Feather name="home" size={24} color="black" />
-        <Tabs.Screen name="index" options={{ headerShown: false , 
+      <Tabs screenOptions={{
+        tabBarActiveTintColor: "#7C6FDC",
+        tabBarStyle: {
+          height: 65,
+          paddingBottom: 5,
+          paddingTop: 5,
+        },
+        tabBarItemStyle: {
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+        tabBarIconStyle: {
+          marginTop: 0,
+        },
+      }}>
+        <Tabs.Screen name="index" options={{ 
+          title: "Home",
+          headerShown: false, 
           tabBarIcon: ({color}) => (
-          <Feather name="home" size={24} color={color} />
+            <Feather name="home" size={24} color={color} />
           ),
         }} />
-        <Feather name="check-circle" size={24} color="black" />
-        <Tabs.Screen name="tasks" options={{ headerShown: false,
+        <Tabs.Screen name="tasks" options={{ 
+          title: "Tasks",
+          headerShown: false,
           tabBarIcon: ({color}) => (
-          <Feather name="check-circle" size={24} color={color} />
+            <Feather name="check-circle" size={24} color={color} />
           ),
         }} />
-        <Feather name="calendar" size={24} color="black" />
-        <Tabs.Screen name="calendar" options={{ title: "Calendar",
+        <Tabs.Screen name="calendar" options={{ 
+          title: "Calendar",
           tabBarIcon: ({color}) => (
-          <Feather name="calendar" size={24} color={color} />
+            <Feather name="calendar" size={24} color={color} />
           ),
         }} />
-        <Feather name="heart" size={24} color="black" />
-        <Tabs.Screen name="care" options={{ title: "Care",
+        <Tabs.Screen name="care" options={{ 
+          title: "Care",
           tabBarIcon: ({color}) => (
-          <Feather name="heart" size={24} color={color} />
+            <Octicons name="person" size={24} color={color} />
           ),
         }} />
-        <Feather name="more-horizontal" size={24} color="black" />
-        <Tabs.Screen name="more" options={{ title: "More",
+        <Tabs.Screen name="more" options={{ 
+          title: "More",
           tabBarIcon: ({color}) => (
-          <Feather name="more-horizontal" size={24} color={color} />
+            <Feather name="more-horizontal" size={24} color={color} />
           ),
         }} />
       </Tabs>
